b55fe6e9a6c8310ccfc3c786dde7fdb0b1ecd303
Роман <p314154265@gmail.com>
2019-10-02 21:09:50 +0300

    Add nomal-selection generation

--- selectionGenerator/NormalSelection.cs
+++ selectionGenerator/NormalSelection.cs
@@ -14,7 +14,28 @@ namespace selectionGenerator
 
         public override void generateSelection()
         {
-            throw new NotImplementedException();
+            Random randa = new Random();
+            double doubleSum = 0;
+
+            int n = 1;
+
+            Console.WriteLine("Input the cout of elements");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input left limit of interval");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input right limit of interval");
+            sigma = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                doubleSum = 0;
+                for (int j = 0; j <= 12; j++) {
+                    doubleSum += randa.NextDouble();
+                }
+                selection.Add(Math.Round((a + Math.Pow(sigma, 2) * (doubleSum - 6)), 3)); //N(a,sigma^2)
+            }
+
         }
     }
 }
