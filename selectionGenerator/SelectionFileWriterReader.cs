114a8b1f81adc9ff467a2da89f21a6c4f358f530
Роман <p314154265@gmail.com>
2019-10-03 16:10:45 +0300

    Add writing and reading selection from .txt file

--- selectionGenerator/SelectionFileWriterReader.cs
+++ selectionGenerator/SelectionFileWriterReader.cs
@@ -11,7 +11,7 @@ namespace EMPI
     class SelectionFileWriterReader
     {
 
-         public void writeSelection(Selection sel) {
+         public void writeSelectionToFile(Selection sel) {
             try
             {
                 string[] lines = sel.ToStringArray();
@@ -45,6 +45,66 @@ namespace EMPI
                 Console.WriteLine(ex.Message);
             }
 
+        }
+        public void readSelectionFromFile(Selection sel)
+        {
+            string line;
+            string[] array = new string[10000];
+            bool isRight = false;
+
+            try {
+                using (StreamReader reader = new StreamReader("Selections.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        if (line.Length == 1) {
+
+                            if (sel.GetType() == typeof(EquableSelection) && line == "1")
+                            {
+                                line = reader.ReadLine();
+                                array = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                                isRight = true;
+
+                                break;
+                            }
+
+                            if (sel.GetType() == typeof(ExponentialSelection) && line == "2")
+                            {
+                                line = reader.ReadLine();
+                                array = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                                isRight = true;
+
+                                break;
+                            }
+                            if (sel.GetType() == typeof(NormalSelection) && line == "3")
+                            {
+                                line = reader.ReadLine();
+                                array = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                                isRight = true;
+
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isRight)
+                    {
+                        sel.AddFromStringArray(array);
+                        return;
+                    }
+
+                    Console.WriteLine("This type of selection in file dont exsits");
+                    
+                }
+            }
+
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
